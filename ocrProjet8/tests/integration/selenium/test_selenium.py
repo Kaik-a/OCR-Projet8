dd081330bde2a20b72ac994c2e0d1f7e29029f95
mehdi.bichari <mehdi.bichari@outscale.com>
2020-10-01 22:01:00 +0200

    Corrected selenium test to use live_server_url

--- ocrProjet8/tests/integration/selenium/test_selenium.py
+++ ocrProjet8/tests/integration/selenium/test_selenium.py
@@ -4,28 +4,40 @@ from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
 
+from catalog.models import Product, Favorite
+from scrapping import NUTELLA
+
 
 class SeleniumBasedTestCase(LiveServerTestCase):
     def setUp(self) -> None:
+        caps = DesiredCapabilities().FIREFOX.copy()
+        caps['marionette'] = True
+
         self.user = User.objects.create_user(
             username='test1',
             password='test1@1234'
         )
-        caps = DesiredCapabilities().FIREFOX.copy()
-        caps['marionette'] = True
+
         self.driver = webdriver.Firefox(
             capabilities=caps,
             firefox_binary='/Applications/Applications/Firefox.app/Contents'
                            '/MacOS/firefox-bin',
-
         )
+
+        Product(
+                product_name_fr='produit bon',
+                nutrition_grade_fr='A',
+                categories_tags=['Pâte à tartiner']
+            ).save()
+
+        Product(**NUTELLA).save()
 
     def test_save_favorite(self):
 
-        self.driver.get('http://127.0.0.1:8000/accounts/login')
+        self.driver.get(self.live_server_url + '/accounts/login')
 
         # login
-        self.driver.find_element_by_id('id_login').send_keys('test1')
+        self.driver.find_element_by_id('id_login').send_keys(self.user.username)
         self.driver.find_element_by_id('id_password').send_keys('test1@1234')
         self.driver.find_element_by_id('login-button').click()
 
@@ -46,11 +58,15 @@ class SeleniumBasedTestCase(LiveServerTestCase):
 
         self.assertEqual(self.driver.title, 'Résultats')
 
+        self.assertEqual(len(Favorite.objects.all().filter(user=self.user)), 0)
+
         # save first product
         self.driver.find_element_by_xpath('//figure/form/button').click()
 
         # go to favorites
         self.driver.find_element_by_id('carrot-logo').click()
+
+        self.assertEqual(len(Favorite.objects.all().filter(user=self.user)), 1)
 
         self.assertEqual(self.driver.title, 'Favoris')
 
