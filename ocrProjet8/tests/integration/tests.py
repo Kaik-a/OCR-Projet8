d0c66c6f0aff3f8e48f4e17e9861f8923ef5e37a
mehdi.bichari <mehdi.bichari@outscale.com>
2020-10-02 13:23:46 +0200

    Added integration tests

--- ocrProjet8/tests/integration/tests.py
+++ ocrProjet8/tests/integration/tests.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from uuid import uuid4
 
 from django.contrib.auth.models import User
 from django.test import TestCase
 
-from catalog.models import Product
+from catalog.models import Product, Favorite
 from scrapping import NUTELLA
 from search.views import ProductAutocomplete
 
@@ -33,14 +34,27 @@ class TestFavorite(TestPattern):
     def test_save_favorite(self):
         qs = ProductAutocomplete().get_queryset()
 
+        Favorite(
+            substitute=qs[0],
+            substitued=qs[1],
+            user=self.user,
+            date=datetime.now()
+        ).save()
+
+        self.assertEqual(len(Favorite.objects.all().filter(user=self.user)), 1)
 
     def test_delete_favorite(self):
-        ...
+        qs = ProductAutocomplete().get_queryset()
 
-    def test_save_favorite_wrong_nutriscore(self):
-        ...
+        Favorite(
+            substitute=qs[0],
+            substitued=qs[1],
+            user=self.user,
+            date=datetime.now()
+        ).save()
 
+        self.assertEqual(len(Favorite.objects.all().filter(user=self.user)), 1)
 
-class TestSearchProduct(TestPattern):
-    def test_find_product_better_nutriscore(self):
-        ...
+        Favorite.objects.get(substitute=qs[0]).delete()
+
+        self.assertEqual(len(Favorite.objects.all().filter(user=self.user)), 0)
