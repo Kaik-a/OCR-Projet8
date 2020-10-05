8cfdb51f1cf8992b19ae449d6b8e706f17dd44ec
mehdi.bichari <mehdi.bichari@outscale.com>
2020-10-05 11:51:51 +0200

    Visual changes

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,20 +3,20 @@ from django import forms
 
 class LoginForm(forms.Form):
     """Class to create form for user."""
-    login = forms.CharField(label="Nom d'utilisateur", max_length=100)
-    password = forms.CharField(label="Mot de passse", widget=forms.PasswordInput)
+    login = forms.CharField(label="Login", max_length=100)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
 
 
 class SubscribeForm(forms.Form):
     """Class to create from for creating user."""
-    login = forms.CharField(label="Nom d'utilisateur", max_length=100)
-    email = forms.EmailField(label="E-mail")
-    password = forms.CharField(label="Mot de passse", widget=forms.PasswordInput)
-    confirm_password = forms.CharField(
-        label="Confirmer le mot de passe", widget=forms.PasswordInput
-    )
-    first_name = forms.CharField(label="Prénom", max_length=50)
+    login = forms.CharField(label="Login", max_length=100)
     last_name = forms.CharField(label="Nom", max_length=120)
+    first_name = forms.CharField(label="Prénom", max_length=50)
+    email = forms.EmailField(label="E-mail")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    confirm_password = forms.CharField(
+        label="Confirmer", widget=forms.PasswordInput
+    )
 
     def clean(self):
         cleaned_data = super(SubscribeForm, self).clean()
--- ocrProjet8/forms.py
+++ ocrProjet8/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    subject = forms.CharField()
-    email = forms.EmailField()
-    message = forms.CharField()
+    subject = forms.CharField(label="Sujet", max_length=100)
+    email = forms.EmailField(label="E-mail", max_length=100)
+    message = forms.CharField(
+        label="Message",
+        widget=forms.TextInput(attrs={'class': 'special', 'size': '40'}))
 
